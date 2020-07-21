8be282928ad7265fd69da84008f60a47461d009c
Michael Evans <michaeljevans113@gmail.com>
2020-07-21 14:52:10 -0600

    Create controller and add new, create, and profile actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def profile
+    @user = User.find(session[:user_id])
+  end
+
+  def new
+  end
+
+  def create
+    new_user = User.new(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect_to '/profile'
+      flash[:success] = 'Registration successful! You are now logged in.'
+    else
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
+  end
+end
