16beaaa668d0a85e9f5c191f8425e838f99c197f
Michael Evans <michaeljevans113@gmail.com>
2020-07-21 15:11:33 -0600

    Add test for incomplete user information when registering

--- spec/features/users/register_spec.rb
+++ spec/features/users/register_spec.rb
@@ -34,4 +34,36 @@ RSpec.describe 'User registration page' do
     expect(page).to have_content("Registration successful! You are now logged in.")
     expect(page).to have_content("Welcome, #{name}!")
   end
+
+  it 'does not allow the visitor to register with incomplete information' do
+    visit '/'
+
+    expect(page).to have_link("Register")
+
+    click_link "Register"
+
+    expect(current_path).to eq('/register')
+
+    name = 'Bob'
+    address = '123 Who Cares Ln'
+    city = 'Denver'
+    state = 'CO'
+    zip = '12345'
+    email = 'me@me.com'
+    password = 'secret'
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password
+
+    click_button "Register"
+
+    expect(current_path).to eq('/register')
+
+    expect(page).to have_content('You must fill out all fields in order to register!')
+  end
 end
