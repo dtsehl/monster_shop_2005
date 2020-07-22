fe287aa3a5e299e42b66fa799a29a694f2bfa884
twarbelow <taija.warbelow@google.com>
2020-07-22 14:02:05 -0700

    refactor sessions controller create action to accommodate sad path for login spec. US14 complete and passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "Hello, #{user.name}, you are now logged in."
-    redirect_to '/profile' if user.role == "user"
-    redirect_to '/merchant/dashboard' if user.role == "merchant"
-    redirect_to '/admin/dashboard' if user.role == "admin"
+    if user.nil? || !user.authenticate(params[:password])
+      flash[:error] = "Log in information incorrect, please try again."
+      redirect_to request.referrer
+    else user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Hello, #{user.name}, you are now logged in."
+      redirect_to '/profile' if user.role == "user"
+      redirect_to '/merchant/dashboard' if user.role == "merchant"
+      redirect_to '/admin/dashboard' if user.role == "admin"
+    end
   end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -46,4 +46,38 @@ RSpec.describe 'User can login' do
     expect(current_path).to eq('/admin/dashboard')
     expect(page).to have_content("Hello, #{a_user.name}, you are now logged in.")
   end
+
+  it 'does not allow user to log in with bad credentials' do
+    a_user = User.create!(name: 'Admin', address: '123 Who Cares Ln', city: 'Denver', state: 'CO', zip: '12345', email: 'admin@me.com', password: 'secret', role: 2)
+
+    visit '/login'
+
+    fill_in :email, with: "BAD@me.com"
+    fill_in :password, with: a_user.password
+
+    click_button "Log In"
+
+    expect(current_path).to eq('/login')
+
+    expect(page).to have_content("Log in information incorrect, please try again.")
+
+    fill_in :email, with: a_user.email
+    fill_in :password, with: "nopety"
+
+    click_button "Log In"
+
+    expect(current_path).to eq('/login')
+
+    expect(page).to have_content("Log in information incorrect, please try again.")
+  end
 end
+
+
+# User Story 14, User cannot log in with bad credentials
+#
+# As a visitor
+# When I visit the login page ("/login")
+# And I submit invalid information
+# Then I am redirected to the login page
+# And I see a flash message that tells me that my credentials were incorrect
+# I am NOT told whether it was my email or password that was incorrect
