93ba26d5926c544539ecf9c43d9b563643bc5056
twarbelow <taija.warbelow@google.com>
2020-07-22 13:19:13 -0700

    define create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    flash[:success] = "Hello, #{user.name}, you are now logged in."
+    redirect_to '/profile' if user.role == "user"
   end
 end
