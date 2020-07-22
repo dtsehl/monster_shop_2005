8097fe2f2bbba570e7e3e2701321ba6f9bf29e37
twarbelow <taija.warbelow@google.com>
2020-07-22 13:34:25 -0700

    create merchant dashboard controller and associated view for index action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,5 +7,7 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     flash[:success] = "Hello, #{user.name}, you are now logged in."
     redirect_to '/profile' if user.role == "user"
+    redirect_to '/merchant/dashboard' if user.role == "merchant"
+    redirect_to '/admin/dashboard' if user.role == "admin"
   end
 end
