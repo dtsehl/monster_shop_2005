73450f48a3b42ccad005e79e77ea182e4981e5bc
twarbelow <taija.warbelow@google.com>
2020-07-23 14:38:47 -0700

    test for item statistics

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -55,5 +55,79 @@ RSpec.describe "Items Index Page" do
 
       expect(current_path).to eq("/items/#{@tire.id}")
     end
+
+    it 'shows an area with statistics' do
+       bike_pump = @meg.items.create(name: "Bike Pump", description: "To pump it up!", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/615GENPCD5L._AC_SX425_.jpg", inventory: 15)
+       bike_chain = @meg.items.create(name: "Bike Chain", description: "Better chainz!", price: 75, image: "https://images-na.ssl-images-amazon.com/images/I/51cafKW0NgL._AC_.jpg", inventory: 75)
+       bike_tool = @meg.items.create(name: "Bike Tool", description: "Make it tight!", price: 30, image: "https://www.rei.com/media/product/718804", inventory: 100)
+
+       kong = @brian.items.create(name: "Kong", description: "Tasty treat!", price: 10, image: "https://images-na.ssl-images-amazon.com/images/I/719dcnCnHfL._AC_SL1500_.jpg", inventory: 50)
+       bed = @brian.items.create(name: "Dog Bed", description: "Sleepy time!", price: 21, image: "https://images-na.ssl-images-amazon.com/images/I/71IvYiQYcAL._AC_SY450_.jpg", inventory: 40)
+
+       name = "Bert"
+       address = "123 Sesame St."
+       city = "NYC"
+       state = "New York"
+       zip = 10001
+
+      8.times do
+        visit "/items/#{@dog_bone.id}"
+        click_on "Add To Cart"
+      end
+      7.times do
+        visit "/items/#{@pull_toy.id}"
+        click_on "Add To Cart"
+      end
+      6.times do
+        visit "/items/#{@tire.id}"
+        click_on "Add To Cart"
+      end
+      5.times do
+        visit "/items/#{bed.id}"
+        click_on "Add To Cart"
+      end
+      4.times do
+        visit "/items/#{kong.id}"
+        click_on "Add To Cart"
+      end
+      3.times do
+        visit "/items/#{bike_tool.id}"
+        click_on "Add To Cart"
+      end
+      2.times do
+        visit "/items/#{bike_chain.id}"
+        click_on "Add To Cart"
+      end
+      visit "/items/#{bike_pump.id}"
+      click_on "Add To Cart"
+
+      visit "/cart"
+      click_on "Checkout"
+
+      fill_in :name, with: name
+      fill_in :address, with: address
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip, with: zip
+
+      click_button "Create Order"
+
+      visit '/items'
+      expect(page).to have_content("Statistics")
+      within ".statistics-top-5" do
+        expect(@dog_bone).to appear_before(@pull_toy)
+        expect(@pull_toy).to appear_before(@tire)
+        expect(@tire).to appear_before(bed)
+        expect(bed).to appear_before(kong)
+        expect(page).to have_content("#{@dog_bone.name}: 8")
+      end
+      within ".statistics-bottom-5" do
+        expect(bike_pump).to appear_before(bike_chain)
+        expect(bike_chain).to appear_before(bike_tool)
+        expect(bike_tool).to appear_before(kong)
+        expect(kong).to appear_before(bed)
+        expect(page).to have_content("#{kong.name}: 4")
+      end
+    end
   end
 end
