ede5eb81b46912ae8bf15e7b75c82b57c4359470
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-23 11:37:20 -0600

    Refactor Users controller; redefine @user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def profile
-    require "pry"; binding.pry
-    @user = User.find(session[:user_id])
+    @user = current_user
+    render file: "/public/404" if !current_user
   end
 
   def new
