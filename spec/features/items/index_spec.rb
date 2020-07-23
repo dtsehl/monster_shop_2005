5db3e9bf15d0d37d097fb834e702eab853d1e14e
Michael Evans <michaeljevans113@gmail.com>
2020-07-23 11:04:30 -0600

    Adjust expectation for clicking image as link

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      expect(page).to have_link(@dog_bone.name)
-      expect(page).to have_link(@dog_bone.merchant.name)
     end
 
     it "I can see a list of all of the items "do
@@ -31,7 +29,6 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_link(@tire.name)
         expect(page).to have_content(@tire.description)
         expect(page).to have_content("Price: $#{@tire.price}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@tire.inventory}")
         expect(page).to have_link(@meg.name)
         expect(page).to have_css("img[src*='#{@tire.image}']")
@@ -41,21 +38,10 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_link(@pull_toy.name)
         expect(page).to have_content(@pull_toy.description)
         expect(page).to have_content("Price: $#{@pull_toy.price}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@pull_toy.inventory}")
         expect(page).to have_link(@brian.name)
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
-
-      # within "#item-#{@dog_bone.id}" do
-      #   expect(page).to have_link(@dog_bone.name)
-      #   expect(page).to have_content(@dog_bone.description)
-      #   expect(page).to have_content("Price: $#{@dog_bone.price}")
-      #   expect(page).to have_content("Inactive")
-      #   expect(page).to have_content("Inventory: #{@dog_bone.inventory}")
-      #   expect(page).to have_link(@brian.name)
-      #   expect(page).to have_css("img[src*='#{@dog_bone.image}']")
-      # end
     end
 
     it 'does not display items that are inactive, and item images are links the item show page' do
@@ -65,11 +51,7 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_content(@pull_toy.name)
       expect(page).to_not have_content(@dog_bone.name)
 
-      within "#item-#{@tire.id}" do
-        within '.imglnk' do
-          click_link
-        end
-      end
+      find("#item-#{@tire.id}-img").click
 
       expect(current_path).to eq("/items/#{@tire.id}")
     end
