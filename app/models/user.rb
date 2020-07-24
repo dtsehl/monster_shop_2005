16d97e5d0f3472a124fb10fae71685c42022b63a
Dan Sehl <dan.sehl@gmail.com>
2020-07-23 18:50:09 -0600

    Change validates_presence_of password to _password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :role
-  validates_presence_of :password, require: true
+  validates_presence_of :password_digest
   validates :email, uniqueness: true, presence: true
 
   has_many :user_orders
