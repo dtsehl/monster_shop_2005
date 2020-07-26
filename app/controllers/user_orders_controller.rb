deca720e1bc6f1c096928c08c0a14ae3c4a276a0
Dan Sehl <dan.sehl@gmail.com>
2020-07-25 19:17:56 -0600

    Add show action to user orders controller

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -4,5 +4,11 @@ class UserOrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:order_id])
+    item_orders = ItemOrder.where("order_id = ?", params[:order_id])
+    @items = item_orders.map do |item_order|
+      Item.find(item_order.item_id)
+    end
+
   end
 end
