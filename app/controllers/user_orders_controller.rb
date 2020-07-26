367c9356c58efe9cf9a0f5ceb53852ec9b3ac066
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-26 16:19:54 -0600

    Add cancel order functionality. Changes item_order status and order status.

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -9,6 +9,19 @@ class UserOrdersController < ApplicationController
     @items = item_orders.map do |item_order|
       Item.find(item_order.item_id)
     end
+  end
 
+  def cancel_order
+    order = Order.find(params[:order_id])
+    order.status = "Cancelled"
+    order.save
+
+    item_orders = order.item_orders
+    item_orders.each do |item_order|
+      item_order.status = "Unfulfilled"
+      item_order.save
+    end
+    redirect_to "/profile"
+    flash[:alert]="Order successfully cancelled"
   end
 end
