8133bdf33edf5010fdea98ffa247a1f348f5f57e
Michael Evans <michaeljevans113@gmail.com>
2020-07-27 15:17:41 -0600

    Add migration for order status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200725223658) do
+ActiveRecord::Schema.define(version: 20200727211617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,30 +14,49 @@ admin = User.create!(name: 'Bob', address: '123 Who Cares Ln', city: 'Denver', s
 merchant = User.create!(name: 'Jim', address: '456 Blah Blah Blvd', city: 'Denver', state: 'CO', zip: '12345', email: 'regularjim@me.com', password: 'alsosecret', role: 1)
 
 #merchants
-bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-dog_shop = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+tire_shop = Merchant.create!(name: 'Rubber, Meet Road', address: '621 Knox St', city: 'Denver', state: 'CO', zip: 80209)
+pot_shop = Merchant.create!(name: 'Green Thumb', address: '420 High St', city: 'Denver', state: 'CO', zip: 80207)
+bike_shop = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+clothes_shop = Merchant.create!(name: 'Lotta Clothes', address: '234 West St', city: 'Denver', state: 'CO', zip: 80208)
+coffee_shop = Merchant.create!(name: 'Bean Roasters', address: '242 Lowell St', city: 'Denver', state: 'CO', zip: 80201)
+dog_shop = Merchant.create!(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
 #bike_shop items
-tire = bike_shop.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+tire = bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+bike_pump = bike_shop.items.create!(name: "Bike Pump", description: "To pump it up!", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/615GENPCD5L._AC_SX425_.jpg", inventory: 15)
+bike_chain = bike_shop.items.create!(name: "Bike Chain", description: "Better chainz!", price: 75, image: "https://images-na.ssl-images-amazon.com/images/I/51cafKW0NgL._AC_.jpg", inventory: 75)
+bike_tool = bike_shop.items.create!(name: "Bike Tool", description: "Make it tight!", price: 30, image: "https://www.rei.com/media/product/718804", inventory: 100)
+lame_clothes = bike_shop.items.create!(name: 'Tight Biker Clothes', description: 'Unnecessary', price: 95, image: 'aslkdfjlsakf', inventory: 9)
 
 #dog_shop items
-pull_toy = dog_shop.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-dog_bone = dog_shop.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+pull_toy = dog_shop.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+dog_bone = dog_shop.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+kong = dog_shop.items.create!(name: "Kong", description: "Tasty treat!", price: 10, image: "https://images-na.ssl-images-amazon.com/images/I/719dcnCnHfL._AC_SL1500_.jpg", inventory: 50)
+bed = dog_shop.items.create!(name: "Dog Bed", description: "Sleepy time!", price: 21, image: "https://images-na.ssl-images-amazon.com/images/I/71IvYiQYcAL._AC_SY450_.jpg", inventory: 40)
 
-bike_pump = bike_shop.items.create(name: "Bike Pump", description: "To pump it up!", price: 25, image: "https://images-na.ssl-images-amazon.com/images/I/615GENPCD5L._AC_SX425_.jpg", inventory: 15)
-bike_chain = bike_shop.items.create(name: "Bike Chain", description: "Better chainz!", price: 75, image: "https://images-na.ssl-images-amazon.com/images/I/51cafKW0NgL._AC_.jpg", inventory: 75)
-bike_tool = bike_shop.items.create(name: "Bike Tool", description: "Make it tight!", price: 30, image: "https://www.rei.com/media/product/718804", inventory: 100)
+#pot_shop items
+nug = pot_shop.items.create!(name: 'Nug', description: 'Huge nug!', price: 15, image: 'skldjfslk', inventory: 20)
+pre_roll = pot_shop.items.create!(name: 'PRJ', description: 'Pre-rolled J', price: 10, image: 'alkjsf', inventory: 15)
+edible = pot_shop.items.create!(name: 'Cosmic Brownie', description: 'Super strong!', price: 5, image: 'aslkdjf', inventory: 80)
 
-kong = dog_shop.items.create(name: "Kong", description: "Tasty treat!", price: 10, image: "https://images-na.ssl-images-amazon.com/images/I/719dcnCnHfL._AC_SL1500_.jpg", inventory: 50)
-bed = dog_shop.items.create(name: "Dog Bed", description: "Sleepy time!", price: 21, image: "https://images-na.ssl-images-amazon.com/images/I/71IvYiQYcAL._AC_SY450_.jpg", inventory: 40)
+#clothes_shop items
+shirt = clothes_shop.items.create!(name: 'White T', description: 'It will stain quickly', price: 10, image: 'sfkjkaljsf', inventory: 25)
+pants = clothes_shop.items.create!(name: 'Khakis', description: 'Light brown', price: 35, image: 'sfkjkaljsf', inventory: 15)
+
+#coffee_shop items
+dark_roast = coffee_shop.items.create!(name: 'Dark Roast', description: 'Like sludge', price: 17, image: 'sfkjkaljsf', inventory: 12)
+light_roast = coffee_shop.items.create!(name: 'Light Roast', description: 'So smooth', price: 15, image: 'sfkjkaljsf', inventory: 16)
+
+#tire_shop items
+mud_tires = tire_shop.items.create!(name: 'Mudders', description: 'You can go anywhere', price: 800, image: 'aoiwejflk', inventory: 8)
 
 order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
 order_2 = Order.create!(name: 'Yo', address: 'Whatever', city: 'Place', state: 'PA', zip: 17033)
 order_1.item_orders.create!(item: pull_toy, price: pull_toy.price, quantity: 4)
-order_2.item_orders.create!(item: pull_toy, price: pull_toy.price, quantity: 3)
 order_1.item_orders.create!(item: tire, price: tire.price, quantity: 6)
-order_2.item_orders.create!(item: bed, price: bed.price, quantity: 5)
-order_1.item_orders.create!(item: kong, price: kong.price, quantity: 4)
 order_1.item_orders.create!(item: bike_tool, price: bike_tool.price, quantity: 3)
+order_1.item_orders.create!(item: kong, price: kong.price, quantity: 4)
+order_2.item_orders.create!(item: pull_toy, price: pull_toy.price, quantity: 3)
+order_2.item_orders.create!(item: bed, price: bed.price, quantity: 5)
 order_2.item_orders.create!(item: bike_chain, price: bike_chain.price, quantity: 2)
 order_2.item_orders.create!(item: bike_pump, price: bike_pump.price, quantity: 1)
