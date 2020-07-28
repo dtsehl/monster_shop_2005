febe2c52c13c157d6c9f82cfc59654e9ebcaebf1
twarbelow <taija.warbelow@google.com>
2020-07-27 17:38:33 -0700

    add merchant relationship to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200727211617) do
+ActiveRecord::Schema.define(version: 20200728003650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20200727211617) do
     t.integer "role", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_users_on_merchant_id"
   end
 
   add_foreign_key "item_orders", "items"
@@ -97,4 +99,5 @@ ActiveRecord::Schema.define(version: 20200727211617) do
   add_foreign_key "reviews", "items"
   add_foreign_key "user_orders", "orders"
   add_foreign_key "user_orders", "users"
+  add_foreign_key "users", "merchants"
 end
