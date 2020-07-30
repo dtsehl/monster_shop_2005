ad7834efbfd404054672d0ecfd03b8c809ce3284
Alex Desjardins <ardesjardins91@gmail.com>
2020-07-30 15:36:51 -0600

    Add sad path functionality to test.

--- spec/features/merchant/edit_item_spec.rb
+++ spec/features/merchant/edit_item_spec.rb
@@ -38,4 +38,42 @@ RSpec.describe 'Merchant items edit' do
     expect(page).to have_content("32")
     expect(page).to have_content("Active")
   end
+
+  it 'when I edit an item and dont fill in all required fields' do
+    dog_shop = Merchant.create!(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
+    merchant = User.create!(name: 'Jim', address: '456 Blah Blah Blvd', city: 'Denver', state: 'CO', zip: '12345', email: 'regularjim@me.com', password: 'alsosecret', role: 1, merchant_id: dog_shop.id)
+    pull_toy = dog_shop.items.create!(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+    dog_bone = dog_shop.items.create!(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", inventory: 21)
+
+    visit '/login'
+    fill_in :email, with: merchant.email
+    fill_in :password, with: merchant.password
+    click_button "Log In"
+
+    visit "/merchant/items"
+    within "#item-#{pull_toy.id}" do
+      click_link "Edit Item"
+    end
+
+    expect(current_path).to eq("/merchant/items/#{pull_toy.id}/edit")
+
+    expect(find_field('Name').value).to eq "Pull Toy"
+    expect(find_field('Price').value).to eq '10'
+    expect(find_field('Description').value).to eq "Great pull toy!"
+    expect(find_field('Image').value).to eq("http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg")
+    expect(find_field('Inventory').value).to eq '32'
+
+    fill_in "Description", with: ""
+
+    click_button "Update Item"
+    expect(page).to have_content("Description can't be blank")
+
+    expect(current_path).to eq("/merchant/items/#{pull_toy.id}/edit")
+
+    expect(find_field('Name').value).to eq "Pull Toy"
+    expect(find_field('Price').value).to eq '10'
+    expect(find_field('Description').value).to eq "Great pull toy!"
+    expect(find_field('Image').value).to eq("http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg")
+    expect(find_field('Inventory').value).to eq '32'
+  end
 end
